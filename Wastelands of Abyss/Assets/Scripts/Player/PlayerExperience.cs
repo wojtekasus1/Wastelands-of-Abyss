9c6e255c07b0d452983e7b8109443b3d1aba45c2
Bartosz Janik <bartekjanik45@gmail.com>
2025-01-18 13:59:45 +0100

    feat: xp script and xp bar

---
+++ Wastelands of Abyss/Assets/Scripts/Player/PlayerExperience.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+public class PlayerExperience : MonoBehaviour
+{
+    public float currentXP = 0;
+    public float requiredXP = 100;
+    public float level = 1;
+
+    public void AddExperience(float amount)
+    {
+        currentXP += amount;
+
+        if (currentXP >= requiredXP)
+        {
+            LevelUp();
+        }
+    }
+
+    private void LevelUp()
+    {
+        level++;
+        currentXP -= requiredXP;
+        requiredXP = Mathf.RoundToInt(requiredXP * 1.2f); // Kolejny poziom zwiekszy req xp o 20%
+        Debug.Log("Level Up! Nowe poziom: " + level);
+    }
+}
