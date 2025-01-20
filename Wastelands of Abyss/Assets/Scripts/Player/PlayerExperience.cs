a4bba9cada02a0108eaba07986c975ef22e2a61c
Bartosz Janik <bartekjanik45@gmail.com>
2025-01-20 12:53:57 +0100

    feat: experience system fully working

--- Wastelands of Abyss/Assets/Scripts/Player/PlayerExperience.cs
+++ Wastelands of Abyss/Assets/Scripts/Player/PlayerExperience.cs
@@ -6,32 +6,35 @@ public class PlayerExperience : MonoBehaviour
     public int currentExp;
     public int requiredXP;
     public int level;
-    public Slider xpSlider;
+    public Material radialMaterial;
     public Text levelText;
 
     public void AddExperience(int amount)
     {
         currentExp += amount;
-        UpdateXPBar();
+
         if (currentExp >= requiredXP)
         {
             LevelUp();
         }
 
+        UpdateXPBar();
     }
 
     private void LevelUp()
     {
         level++;
         currentExp -= requiredXP;
-        requiredXP = Mathf.RoundToInt(requiredXP * 1.2f); //Zwiekszenie wymaganego xp o 20% poprzedniej wartosci
+        requiredXP = Mathf.RoundToInt(requiredXP * 1.2f); 
         Debug.Log("Level Up! Nowy poziom: " + level);
-
-        UpdateXPBar();
     }
+
     private void UpdateXPBar()
     {
-        xpSlider.value = (float)currentExp / requiredXP;  // Aktualizowanie paska XP
-        levelText.text = "Level: " + level;              // Aktualizowanie tekstu z poziomem
+        float progress = (float)currentExp / requiredXP;
+
+        radialMaterial.SetFloat("_Progress", progress);
+
+        levelText.text = "Level: " + level;
     }
 }
